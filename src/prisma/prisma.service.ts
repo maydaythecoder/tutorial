89fa065187b877819009473f533eb3da1482d009
mayday <maydaythecoder@gmail.com>
2025-08-15 00:33:15 -0400

    error resolution in prisma module

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,10 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../../generated/prisma';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor() {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
     super({
       datasources: {
         db: {
