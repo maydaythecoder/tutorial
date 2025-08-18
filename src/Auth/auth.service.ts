24344c8b3373020d71b78941e2a31fe6f53ee524
mayday <maydaythecoder@gmail.com>
2025-08-17 20:08:54 -0400

    Add TypeScript configuration, error handling, and Prisma integration in Auth module

--- src/Auth/auth.service.ts
+++ src/Auth/auth.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { SignInDto, SignUpDto } from './dto/auth.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
   signup(dto: SignUpDto) {
     return `Signing up user with email: ${dto.email}`;
   }
